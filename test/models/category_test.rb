c2d79552d9330e133c38cb745cd65419692129d2
kchn9 <mat.kuchnia@gmail.com>
2022-07-18 07:53:43 +0200

    Create validation categories test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,10 +2,33 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: "Sports")
+  end
+
+  test "category should be valid" do
     assert @category.valid?
   end
 
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    Category.create(name: "news")
+    @category.name = "News"
+    assert_not @category.valid?
+  end
+
+  test "name should be not too long" do
+    @category.name = "A" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should be not too short" do
+    @category.name = "A" * 2
+    assert_not @category.valid?
+  end
 
 end
