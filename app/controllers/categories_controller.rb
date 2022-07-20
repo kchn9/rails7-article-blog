5d4dfdc8655a23cbad8fe95c05d27eacfb10e787
kchn9 <mat.kuchnia@gmail.com>
2022-07-20 08:57:38 +0200

    Add create and test controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,12 +5,21 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category was succesfully created"
+      redirect_to @category
+    else
+      render :new
+    end
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   # def destroy
@@ -18,5 +27,10 @@ class CategoriesController < ApplicationController
 
   # def edit
   # end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
 end
