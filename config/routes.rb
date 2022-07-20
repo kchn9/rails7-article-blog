1112095f1d9365405f4a7daae3d907c02bce8f96
kchn9 <mat.kuchnia@gmail.com>
2022-07-20 08:24:09 +0200

    Add routes.rb comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root "articles#index"
+  # users
   match "signup", :to => 'users#create', :via => 'post'
   get "signup", to: "users#new"
+  # session
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "login", to: "sessions#destroy"
+  # resources
   resources :users, except: [:new]
   resources :articles
   resources :categories, except: [:destroy, :edit]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
